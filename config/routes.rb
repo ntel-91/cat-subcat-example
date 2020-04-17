c016ebec25df46930f6294fa6c33d53f7fd81cc7
ntel-91 <ntelenson@gmail.com>
2020-04-17 16:31:51 -0400

    added root page w categories

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :categories
+  
+  root to: 'categories#index'
+  # resources :products
+  # resources :categories
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ProductCategory.destroy_all
+Product.destroy_all
+Category.destroy_all
+
+CATEGORIES = Faker::Base.fetch_all('commerce.department').map do |name|
+    Category.find_or_create_by!(name: name)
+end
